9ab03578ce45202d2d2840d71775af90f03942ae
Tommasobondi <80787909+Tommasobondi@users.noreply.github.com>
2022-10-20 16:09:33 +0300

    Add files via upload

--- Download_Sentinel.py
+++ Download_Sentinel.py
@@ -10,45 +10,7 @@ from geojson import Polygon
 import sys
 
 
-theWD = 'import sentinelsat
-from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-from datetime import date
-
-
-import geojson    
-import pandas as pd
-#from area import area
-from geojson import Polygon
-import sys
-
-
-theWD = 'C:\Users\geom21020\Desktop\Sentinel\'
-
-Xman = 26.55345
-Ymax = 39.14115
-
-Xmin = 26.53545
-Ymin = 39.12395
-
-d = {'X': [26.53545, 26.55345, 26.55345, 26.53545], 'Y': [39.12395, 39.12395, 39.14115, 39.14115]}
-df = pd.DataFrame(data=d)
-
-long_coords = df['X'].tolist()
-lat_coords = df['Y'].tolist()
-
-data = []
-for i in range(0, len(long_coords)):
-    data.append( tuple([long_coords[i],lat_coords[i]]) )
-
-data.append(data[0])
-
-print("Total GPS Coordinates: {}".format(len(data)))
-
-obj = Polygon([data])  
-    
-f = open(theWD + "\AOI.json", "w")
-f.write(str(obj))
-f.close()'
+theWD = 'C:\\Temp\\Sentinel\\'
 
 Xman = 26.55345
 Ymax = 39.14115
