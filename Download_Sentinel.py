249de23324432dbf5b30e4683899b64403601089
Tommasobondi <80787909+Tommasobondi@users.noreply.github.com>
2022-10-21 13:03:46 +0300

    Add files via upload

--- Download_Sentinel.py
+++ Download_Sentinel.py
@@ -2,19 +2,15 @@ import sentinelsat
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
 
+
 import geojson    
 import pandas as pd
 #from area import area
 from geojson import Polygon
 import sys
-import pyunpack
-import os
-import zipfile
 
-theWD = 'C:\\Users\\geom21020\\Desktop\\Sentinel'  #mywd
 
-#intrest area coordinates
-#build the geojson file
+theWD = 'C:\\Temp\\Sentinel\\'
 
 Xman = 26.55345
 Ymax = 39.14115
@@ -22,7 +18,7 @@ Ymax = 39.14115
 Xmin = 26.53545
 Ymin = 39.12395
 
-d = {'X': [26.53545, 26.55345, 26.55345, 26.53545], 'Y': [39.12395, 39.12395, 39.14115, 39.14115]} 
+d = {'X': [26.53545, 26.55345, 26.55345, 26.53545], 'Y': [39.12395, 39.12395, 39.14115, 39.14115]}
 df = pd.DataFrame(data=d)
 
 long_coords = df['X'].tolist()
@@ -42,45 +38,21 @@ f = open(theWD + "\AOI.json", "w")
 f.write(str(obj))
 f.close()
 
-#download immagines
 
 
-api = SentinelAPI('tommasobondi', 'francedo03!', 'https://scihub.copernicus.eu/dhus')  #mydata to accede
-footprint = geojson_to_wkt(read_geojson(theWD + '\AOI.json'))
+
+api = SentinelAPI('tommasobondi', 'francedo03!', 'https://scihub.copernicus.eu/dhus') #mypassandusername
+footprint = geojson_to_wkt(read_geojson(theWD + '\AOI.json')) #my directory, file created with qgis, creating a polygon and extracting the shapefine in geojson
 
 directory_path = ''
 
 products = api.query(footprint,
-                       date=('20211020', '20211120'),  #date
-                       platformname='Sentinel-2',      #platform
-                       cloudcoverpercentage=(0, 20),    #first cloud mask
-                       producttype='S2MSI1C')        #prodoucttype
+                       date=('20211010', '20211020'), #+-, don't look so mutch but the amount is still and the pc block
+                       platformname='Sentinel-2',
+                       cloudcoverpercentage=(0, 20),
+                       producttype='S2MSI1C') #ortheconvenient one, but it depends if the percentege is of the file or the area
+                       #prodouctlevel='Level-1C' (alreadyeleborated one)
 
 
 
-api.download_all(products, theWD)
-
-#extract and delete the compressed
-
-for root, dirs, files in os.walk(theWD):
-    for filename in files:
-        if filename.endswith(".rar") :
-            print('RAR:'+os.path.join(root,filename))
-        elif filename.endswith(".zip"):
-            print('ZIP:'+os.path.join(root,filename))
-        name = os.path.splitext(os.path.basename(filename))[0]
-        if filename.endswith(".rar") or filename.endswith(".zip"):
-            try:
-                arch = pyunpack.Archive(os.path.join(root,filename))
-                # os.mkdir(name)
-                arch.extractall(directory=root)
-                os.remove(os.path.join(root,filename))
-            except Exception as e:
-                print("ERROR: BAD ARCHIVE "+os.path.join(root,filename))
-                print(e)
-                try:
-                    pass
-                except OSError as e:
-                    if e.errno != errno.ENOENT:
-                        raise
-                        
+api.download_all(products, directory_path)
